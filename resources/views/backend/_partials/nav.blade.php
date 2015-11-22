995c6a529a0134199ef396f8d83ddd9bcf42ec65
hiendv <neo.go.vn@gmail.com>
2015-11-22 01:30:48 +0000

    minor fix

--- resources/views/backend/_partials/nav.blade.php
+++ resources/views/backend/_partials/nav.blade.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">Feedback</a>
+      <a class="navbar-brand" href="{{ route('backend.dashboard') }}">Feedback</a>
     </div>
     <div id="navbar" class="collapse navbar-collapse">
       <ul class="nav navbar-nav navbar-right">
--- resources/views/frontend/_partials/nav.blade.php
+++ resources/views/frontend/_partials/nav.blade.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">Feedback</a>
+      <a class="navbar-brand" href="{{route('frontend.welcome')}}">Feedback</a>
     </div>
     <div id="navbar" class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
